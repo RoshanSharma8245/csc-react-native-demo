f81b6d3dfd07c8dcd9050812c406c981ad1191e6
Roshan Kumar Sharma <roshan@conscent.ai>
2024-01-09 14:48:01 +0530

    auto login using google login

--- src/screen/ContentScreen.js
+++ src/screen/ContentScreen.js
@@ -4,9 +4,11 @@ import { Alert, SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native'
 
 //PACKAGES
 import { useFocusEffect } from '@react-navigation/native';
-import PayWall,{getEventsEnvDetails, pageExist} from 'csc-react-native-sdk';
+import PayWall, { getEventsEnvDetails, pageExist, onTouchListener } from 'csc-react-native-sdk';
 import DeviceInfo from "react-native-device-info";
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+import { autoLoginView, genrateTempToken } from './api';
+
 
 GoogleSignin.configure({
     webClientId: '784024490654-r5htgk5oletn228deq8fh6s85hsdn0pg.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
@@ -26,7 +28,7 @@ export default function ContentScreen(props) {
     const { contentId, clientId, mode } = props?.route?.params
 
     const text = ['Where does it come from?\n\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.']
-
+    const premiumContent = ['Content Unlock \n Read premium content'];
     useEffect(() => {
         getUserAgent()
     }, [])
@@ -71,7 +73,16 @@ export default function ContentScreen(props) {
         try {
             await GoogleSignin.hasPlayServices();
             const userInfo = await GoogleSignin.signIn();
-            console.log(userInfo);
+
+            const email = userInfo.user.email;
+
+            console.log(email);
+
+            const data = await genrateTempToken(email, mode);
+            console.log(data);
+            const tempToken = data?.tempAuthToken;
+
+            await autoLoginView(tempToken, clientId, email, "", "ContentScreen", props.navigation, mode);
 
 
 
@@ -110,8 +121,12 @@ export default function ContentScreen(props) {
             <ScrollView
                 onScroll={(e) => {
                     setScrollY(e.nativeEvent.contentOffset.y)
+                    onTouchListener();
                 }}>
-                <Text>{text[0] + "\n\n" + text[0] + "\n\n" + text[0]}</Text>
+                {
+                    showContent ?
+                        <Text>{premiumContent[0] + "\n\n" + text[0] + "\n\n" + text[0]}</Text> : <Text>{text[0] + "\n\n" + text[0] + "\n\n" + text[0]}</Text>
+                }
             </ScrollView>
             {userAgent && showPaywall &&
                 <PayWall
---
+++ src/screen/api.js
@@ -0,0 +1,61 @@
+import { getEnvDetails, getLoginChallengeId, getServiceEnvDetails } from 'csc-react-native-sdk';
+import base64 from 'react-native-base64'
+
+export const genrateTempToken = async (email, mode) => {
+    // let base64 = require("base-64"); // install it before use from npm i base-64
+
+    const username = "J1EFAQR-H0N4921-QCXKVNH-6W9ZYY9";
+    const password = "CFR472795Q42TTQJFV84M37A5G4SJ1EFAQRH0N4921QCXKVNH6W9ZYY9";
+
+    //function for Fetching data from API
+    const API_BASE_URL = getEnvDetails(mode);
+    const url = `${API_BASE_URL}/client/generate-temp-token`;
+    const raw = JSON.stringify({
+        "email": email
+    });
+    console.log(raw);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: "Basic " + base64.encode(username + ":" + password),
+            },
+            body: raw,
+        });
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+
+    }
+
+}
+
+export const autoLoginView = async (tempToken, clientId, email, phoneNumber, currentStackName, navigation, mode) => {
+
+    const API_BASE_URL = getEnvDetails(mode);
+    const loginChallengeId = await getLoginChallengeId(API_BASE_URL);
+    const FRONT_END_BASE_URL = getServiceEnvDetails(mode);
+    try {
+
+        const REDIRECT_URL = `${FRONT_END_BASE_URL}/auto-login-user?id=${tempToken}&clientId=${clientId}&phone=${phoneNumber}&email=${email}&loginChallengeId=${loginChallengeId}`
+
+
+        navigation.navigate('ConscentWebView', {
+            REDIRECT_URL: REDIRECT_URL,
+            currentStackName: currentStackName,
+            mode
+        });
+
+        // return REDIRECT_URL;
+    } catch (error) {
+        // if (Conscent._app_mode == AppMode.DEBUG) {
+        //     console.warn(error);
+        // }
+    }
+
+}
